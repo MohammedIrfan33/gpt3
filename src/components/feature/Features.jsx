9ea68e0d2266b6a0c1ad65ccfaa9a95a7c37c88f
irfancrudx <apirfan33@gmail.com>
2023-01-30 22:37:27 +0530

    Initial commit

---
+++ src/components/feature/Features.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './feature.css';
+
+const Features = () => {
+  return (
+    <div>features</div>
+  )
+}
+
+export default Features
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export {default as Article} from './articles/Articles';
+export {default as Brand} from './brand/Brand';
+export {default as  CTA } from './cta/Cta';
+export {default as Features} from './feature/Features'
+export {default as  Navbar } from './navbar/Navbar';
+
+
